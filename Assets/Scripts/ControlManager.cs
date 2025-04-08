ba5f0f4e67dda5c8f292bb83898fdec21cd1c8d0
madBennett <bennettmadison427@gmail.com>
2025-04-08 14:14:08 -0700

    Add Player Movement and Basic Networking

---
+++ Assets/Scripts/ControlManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class ControlManager : MonoBehaviour
+{
+    //Movement
+    [SerializeField] private float normSpeed = 5f;
+    private float currSpeed;
+    private Vector2 movement;
+
+    [SerializeField] private GameObject PlayerObj;
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currSpeed = normSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the player based on user input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //move the player according to inputs on the server
+        MoveServerRpc(movement, currSpeed);
+
+        //flash light
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+
+    [ServerRpc]
+    public void MoveServerRpc(Vector2 movement, float curSpeed)
+    {
+        //get componates for movement
+        rigidBody = GetComponent<Rigidbody2D>();
+        //prevent odd input
+        movement.Normalize();
+
+        //move through rigid body
+        rigidBody.velocity = movement * currSpeed;
+
+        RotatePlayer();
+    }
+
+    void RotatePlayer()
+    {
+        //rotate player accordingly
+        if (movement.x != 0)
+        {
+            //rotate left or right
+            PlayerObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, movement.x * -90));
+        }
+
+        if (movement.y == 1)
+        {
+            //rotate up
+            PlayerObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        else if (movement.y == -1)
+        {
+            //rotate down
+            PlayerObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+    }
+}
