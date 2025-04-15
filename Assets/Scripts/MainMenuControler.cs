8ffe1831000f622bf98cc35c8b17eb62381b53de
madBennett <bennettmadison427@gmail.com>
2025-04-15 01:32:42 -0700

    Refactor File Structure and Add Test Scenes to Lobby Menu
    -Move prefabs to assests folder

--- Assets/Scripts/MainMenuControler.cs
+++ Assets/Scripts/MainMenuControler.cs
@@ -8,10 +8,17 @@ public class MainMenuControler : NetworkBehaviour
 {
     string mainGameSceneName = "Main Game";
 
+    string currentSceneName;
+
+    //test Scenes
+    public GameObject TestMenu;
+    string[] testSceneNames = { "Algharbi_Test Scene", "Bennett_Test Scene", "Martinez_Test Scene", "Potter_Test Scene" };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSceneName = mainGameSceneName;
+        TestMenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,7 +33,7 @@ public class MainMenuControler : NetworkBehaviour
         NetworkManager.Singleton.StartHost();
 
         //load game scene
-        NetworkManager.Singleton.SceneManager.LoadScene(mainGameSceneName, LoadSceneMode.Single);
+        NetworkManager.Singleton.SceneManager.LoadScene(currentSceneName, LoadSceneMode.Single);
     }
     public void StartClient()
     {
@@ -34,6 +41,39 @@ public class MainMenuControler : NetworkBehaviour
         NetworkManager.Singleton.StartClient();
         
             //load game scene
-            NetworkManager.Singleton.SceneManager.LoadScene(mainGameSceneName, LoadSceneMode.Single);
+            NetworkManager.Singleton.SceneManager.LoadScene(currentSceneName, LoadSceneMode.Single);
+    }
+
+    //test scene controls
+    public void OpenTestMenu()
+    {
+        TestMenu.SetActive(true);
+    }
+
+    public void CloseTestMenu()
+    {
+        currentSceneName = mainGameSceneName;
+        TestMenu.SetActive(false);
+    }
+
+    public void StartTestScene1()
+    {
+        currentSceneName = testSceneNames[0];
+        StartHost();
+    }
+    public void StartTestScene2()
+    {
+        currentSceneName = testSceneNames[1];
+        StartHost();
+    }
+    public void StartTestScene3()
+    {
+        currentSceneName = testSceneNames[2];
+        StartHost();
+    }
+    public void StartTestScene4()
+    {
+        currentSceneName = testSceneNames[3];
+        StartHost();
     }
 }
