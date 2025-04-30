885185627faa1d6b6c444dbba3db59b66a596762
Afnan Algharbi <146122233+AfnanAlgharbi@users.noreply.github.com>
2025-04-29 20:59:31 -0700

    Lobby system
    I added the lobby system transitions with the doors (locking/ unlocking). I added a red sprite prefab into each scene to mark the completion of a task to trigger the final gate, you guys play with it however you like.

---
+++ Assets/Scripts/Added code/PlayerProgress.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour
+{
+    public static PlayerProgress Instance;
+
+    private HashSet<string> completedTasks = new HashSet<string>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CompleteTask(string taskID)
+    {
+        completedTasks.Add(taskID);
+    }
+
+    public bool HasCompletedTask(string taskID)
+    {
+        return completedTasks.Contains(taskID);
+    }
+}
