3be6d6f9dd0536106d10495d765d6d6466093b06
Afnan Algharbi <146122233+AfnanAlgharbi@users.noreply.github.com>
2025-05-08 18:45:38 -0700

    commit
    added a flame asset and debugging the doors

--- Assets/Escape Room/scripts/KeyAndDoorController.cs
+++ Assets/Escape Room/scripts/KeyAndDoorController.cs
@@ -4,22 +4,30 @@ public class KeyAndDoorController : MonoBehaviour
 {
     private bool hasKey = false;
 
-    public GameObject keyObject;   // Drag the key GameObject here
-    public GameObject doorObject;  // Drag the door GameObject here
+    public GameObject keyObject;
+    public GameObject doorObject;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Collided with: " + other.name); // NEW
+
         if (other.CompareTag("Key"))
         {
             hasKey = true;
-            keyObject.SetActive(false); // Make the key disappear
+            keyObject.SetActive(false);
+            Debug.Log("Key collected!"); // NEW
         }
 
         if (other.CompareTag("Door"))
         {
             if (hasKey)
             {
-                doorObject.SetActive(false); // Open the door
+                doorObject.SetActive(false);
+                Debug.Log("Door unlocked!"); // NEW
+            }
+            else
+            {
+                Debug.Log("Touched door, but no key yet."); // NEW
             }
         }
     }
